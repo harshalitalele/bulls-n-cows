a0895a8d7986c59fc49dc91fd06fe0912bab4905
harshalitalele <harshalitalele@gmail.com>
2019-06-02 15:07:18 +0530

    basic html without css

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-var wordsList = ['Apple', 'Banana'];
+var wordsList = ['Apple', 'Banana'],
+    guessWord = '';
+
+function displayWordInput(wordLen) {
+    var container = document.getElementById('word-input'),
+        i = 0;
+    
+    for(i = 0; i < wordLen; i++) {
+        var elem = getCharacterElem();
+        container.appendChild(elem);
+    }
+}
+
+function getCharacterElem() {
+    var charElem = document.createElement('input');
+    
+    charElem.setAttribute('type', 'text');
+    charElem.setAttribute('maxLength', 1);
+    
+    return charElem;
+}
+
+displayWordInput(4);
